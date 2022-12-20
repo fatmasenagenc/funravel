7d2f16493ac6ac6b18c3480dcca6bbc36ce5e95d
Gülin Yılmaz <64426686+gulinyilmaz@users.noreply.github.com>
2022-12-20 17:45:51 +0300

    calendar and location
    ui update

--- lib/screens/suggested_plan_screen.dart
+++ lib/screens/suggested_plan_screen.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-
 import '../constants/contants.dart';
-
+import 'package:funravel_v0/screens/select_date_screen.dart';
+import 'package:funravel_v0/screens/location_screen.dart';
 class SuggestPlan extends StatefulWidget {
   const SuggestPlan({Key? key}) : super(key: key);
 
@@ -18,6 +18,11 @@ class _SuggestPlan extends State<SuggestPlan> {
 
 
   }
+
+  String dropdownValue = 'Plane';
+
+  @override
+
 
   @override
   Widget build(BuildContext context) {
@@ -44,75 +49,27 @@ class _SuggestPlan extends State<SuggestPlan> {
               height: 40,
             ),
             Container(
-              width: double.infinity,
-              padding: EdgeInsets.only(right: 20, left: 10),
-              margin: EdgeInsets.symmetric(horizontal: 10),
-              color: kSecondaryColor,
-              child: TextField(
-                controller: TextEditingController(),
-                style: TextStyle(
-                    color: kPrimaryDarkenedColor, fontWeight: FontWeight.bold),
-                cursorColor: Colors.black,
-                onChanged: (input) {
-                  print("changed $input");
-                },
-                decoration: InputDecoration(
-                    focusedBorder: UnderlineInputBorder(
-                      borderSide: BorderSide(
-                          width: 0, color: Colors.white.withOpacity(0)),
-                    ),
-                    enabledBorder: UnderlineInputBorder(
-                      borderSide: BorderSide(
-                          width: 0,
-                          color: Colors.white.withOpacity(0)), //<-- SEE HERE
-                    ),
-                    border: UnderlineInputBorder(
-                      borderSide: BorderSide(
-                          width: 2,
-                          color: Colors.white.withOpacity(0)), //<-- SEE HERE
-                    ),
-                    hintText: "Select location",
-                    hintStyle: TextStyle(
-                        color: kPrimaryDarkenedColor.withOpacity(0.5),
-                        fontWeight: FontWeight.bold)),
-              ),
+              child: buildCustomButton(title: "Select Location", function: (){
+
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => Location()));
+
+              },),
+
             ),
             SizedBox(
               height: 20,
             ),
             Container(
-              width: double.infinity,
-              padding: EdgeInsets.only(right: 20, left: 10),
-              margin: EdgeInsets.symmetric(horizontal: 10),
-              color: kSecondaryColor,
-              child: TextField(
-                controller: TextEditingController(),
-                style: TextStyle(
-                    color: kPrimaryDarkenedColor, fontWeight: FontWeight.bold),
-                cursorColor: Colors.black,
-                onChanged: (input) {
-                  print("changed $input");
-                },
-                decoration: InputDecoration(
-                    focusedBorder: UnderlineInputBorder(
-                      borderSide: BorderSide(
-                          width: 0, color: Colors.white.withOpacity(0)),
-                    ),
-                    enabledBorder: UnderlineInputBorder(
-                      borderSide: BorderSide(
-                          width: 0,
-                          color: Colors.white.withOpacity(0)), //<-- SEE HERE
-                    ),
-                    border: UnderlineInputBorder(
-                      borderSide: BorderSide(
-                          width: 2,
-                          color: Colors.white.withOpacity(0)), //<-- SEE HERE
-                    ),
-                    hintText: "Select Start - End Date",
-                    hintStyle: TextStyle(
-                        color: kPrimaryDarkenedColor.withOpacity(0.5),
-                        fontWeight: FontWeight.bold)),
-              ),
+              child: buildCustomButton(title: "Select start-end date", function: (){
+
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => SelectDate()));
+
+              },),
+
             ),
             SizedBox(
               height: 20,
@@ -159,33 +116,31 @@ class _SuggestPlan extends State<SuggestPlan> {
               padding: EdgeInsets.only(right: 20, left: 10),
               margin: EdgeInsets.symmetric(horizontal: 10),
               color: kSecondaryColor,
-              child: TextField(
-                controller: TextEditingController(),
-                style: TextStyle(
-                    color: kPrimaryDarkenedColor, fontWeight: FontWeight.bold),
-                cursorColor: Colors.black,
-                onChanged: (input) {
-                  print("changed $input");
+
+              child: DropdownButton<String>(
+                // Step 3
+                dropdownColor: kSecondaryColor,
+                value: dropdownValue,
+                // Step 4.
+                items: <String>['Plane', 'Train', 'Bus']
+                    .map<DropdownMenuItem<String>>((String value) {
+                  return DropdownMenuItem<String>(
+                    value: value,
+                    child: Text(
+                      value,
+                      style: TextStyle(
+                          fontSize: 15,
+                          color: kPrimaryDarkenedColor.withOpacity(0.5),
+                          fontWeight: FontWeight.bold),
+                    ),
+                  );
+                }).toList(),
+                // Step 5.
+                onChanged: (String? newValue) {
+                  setState(() {
+                    dropdownValue = newValue!;
+                  });
                 },
-                decoration: InputDecoration(
-                    focusedBorder: UnderlineInputBorder(
-                      borderSide: BorderSide(
-                          width: 0, color: Colors.white.withOpacity(0)),
-                    ),
-                    enabledBorder: UnderlineInputBorder(
-                      borderSide: BorderSide(
-                          width: 0,
-                          color: Colors.white.withOpacity(0)), //<-- SEE HERE
-                    ),
-                    border: UnderlineInputBorder(
-                      borderSide: BorderSide(
-                          width: 2,
-                          color: Colors.white.withOpacity(0)), //<-- SEE HERE
-                    ),
-                    hintText: "Select Transportation Type",
-                    hintStyle: TextStyle(
-                        color: kPrimaryDarkenedColor.withOpacity(0.5),
-                        fontWeight: FontWeight.bold)),
               ),
 
 
@@ -236,3 +191,23 @@ class _SuggestPlan extends State<SuggestPlan> {
     );
   }
 }
+
+buildCustomButton({ String? title, Function? function, Widget? sideWidget}){
+  return InkWell(
+    onTap: () => function != null ? function() : null,
+    child: Container(
+      padding: EdgeInsets.all(15),
+      margin: EdgeInsets.only(right: 35, left: 35),
+      color: kSecondaryColor,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(title ?? "", style: TextStyle(color: kPrimaryDarkenedColor, fontSize: 15, fontWeight: FontWeight.bold), ),
+          SizedBox(width: 4,),
+          sideWidget ?? Container()
+        ],
+      ),
+    ),
+  );
+}
+
