0d6bb4155ef0af9239ba3349eb0647d2c83bccc9
Mustafa Efe Tamyapar <efe.tamyapar@ug.bilkent.edu.tr>
2022-12-20 23:23:12 +0300

    Gulins Changes
    Gulins Changes

--- lib/screens/suggested_plan_screen.dart
+++ lib/screens/suggested_plan_screen.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-
 import '../constants/contants.dart';
-
+import 'package:funravel_v0/screens/select_date_screen.dart';
+import 'package:funravel_v0/screens/location_screen.dart';
 class SuggestPlan extends StatefulWidget {
   const SuggestPlan({Key? key}) : super(key: key);
 
@@ -18,6 +18,11 @@ class _SuggestPlan extends State<SuggestPlan> {
 
 
   }
+
+  String dropdownValue = 'Plane';
+
+  @override
+
 
   @override
   Widget build(BuildContext context) {
@@ -44,75 +49,27 @@ class _SuggestPlan extends State<SuggestPlan> {
               height: 40,
             ),
             Container(
-              width: double.infinity,
-              padding: EdgeInsets.only(right: 20, left: 10),
-              margin: EdgeInsets.symmetric(horizontal: 10),
-              color: kSecondaryColor,
-              child: TextField(
-                controller: TextEditingController(),
-                style: TextStyle(
-                    color: kPrimaryDarkenedColor, fontWeight: FontWeight.bold),
-                cursorColor: Colors.black,
-                onChanged: (input) {
-                  print("changed $input");
-                },
-                decoration: InputDecoration(
-                    focusedBorder: UnderlineInputBorder(
-                      borderSide: BorderSide(
-                          width: 0, color: Colors.white.withOpacity(0)),
-                    ),
-                    enabledBorder: UnderlineInputBorder(
-                      borderSide: BorderSide(
-                          width: 0,
-                          color: Colors.white.withOpacity(0)), //<-- SEE HERE
-                    ),
-                    border: UnderlineInputBorder(
-                      borderSide: BorderSide(
-                          width: 2,
-                          color: Colors.white.withOpacity(0)), //<-- SEE HERE
-                    ),
-                    hintText: "Select location",
-                    hintStyle: TextStyle(
-                        color: kPrimaryDarkenedColor.withOpacity(0.5),
-                        fontWeight: FontWeight.bold)),
-              ),
+              child: buildCustomButton(title: "Select Location", function: (){
+
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => Location()));
+
+              },),
+
             ),
             SizedBox(
               height: 20,
             ),
             Container(
-              width: double.infinity,
-              padding: EdgeInsets.only(right: 20, left: 10),
-              margin: EdgeInsets.symmetric(horizontal: 10),
-              color: kSecondaryColor,
-              child: TextField(
-                controller: TextEditingController(),
-                style: TextStyle(
-                    color: kPrimaryDarkenedColor, fontWeight: FontWeight.bold),
-                cursorColor: Colors.black,
-                onChanged: (input) {
-                  print("changed $input");
-                },
-                decoration: InputDecoration(
-                    focusedBorder: UnderlineInputBorder(
-                      borderSide: BorderSide(
-                          width: 0, color: Colors.white.withOpacity(0)),
-                    ),
-                    enabledBorder: UnderlineInputBorder(
-                      borderSide: BorderSide(
-                          width: 0,
-                          color: Colors.white.withOpacity(0)), //<-- SEE HERE
-                    ),
-                    border: UnderlineInputBorder(
-                      borderSide: BorderSide(
-                          width: 2,
-                          color: Colors.white.withOpacity(0)), //<-- SEE HERE
-                    ),
-                    hintText: "Select Start - End Date",
-                    hintStyle: TextStyle(
-                        color: kPrimaryDarkenedColor.withOpacity(0.5),
-                        fontWeight: FontWeight.bold)),
-              ),
+              child: buildCustomButton(title: "Select start-end date", function: (){
+
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => SelectDate()));
+
+              },),
+
             ),
             SizedBox(
               height: 20,
@@ -155,37 +112,51 @@ class _SuggestPlan extends State<SuggestPlan> {
               height: 20,
             ),
             Container(
+              child: Row(
+                children:[
+                  Text('Transportation',style: TextStyle(
+                    color: kSecondaryColor,
+                    fontSize:18,
+                    fontWeight: FontWeight.bold,
+
+                  ),),
+
+                ],
+              ),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Container(
               width: double.infinity,
               padding: EdgeInsets.only(right: 20, left: 10),
               margin: EdgeInsets.symmetric(horizontal: 10),
               color: kSecondaryColor,
-              child: TextField(
-                controller: TextEditingController(),
-                style: TextStyle(
-                    color: kPrimaryDarkenedColor, fontWeight: FontWeight.bold),
-                cursorColor: Colors.black,
-                onChanged: (input) {
-                  print("changed $input");
+
+              child: DropdownButton<String>(
+                // Step 3
+                dropdownColor: kSecondaryColor,
+                value: dropdownValue,
+                // Step 4.
+                items: <String>['Plane', 'Train', 'Bus']
+                    .map<DropdownMenuItem<String>>((String value) {
+                  return DropdownMenuItem<String>(
+                    value: value,
+                    child: Text(
+                      value,
+                      style: TextStyle(
+                          fontSize: 15,
+                          color: kPrimaryDarkenedColor.withOpacity(0.5),
+                          fontWeight: FontWeight.bold),
+                    ),
+                  );
+                }).toList(),
+                // Step 5.
+                onChanged: (String? newValue) {
+                  setState(() {
+                    dropdownValue = newValue!;
+                  });
                 },
-                decoration: InputDecoration(
-                    focusedBorder: UnderlineInputBorder(
-                      borderSide: BorderSide(
-                          width: 0, color: Colors.white.withOpacity(0)),
-                    ),
-                    enabledBorder: UnderlineInputBorder(
-                      borderSide: BorderSide(
-                          width: 0,
-                          color: Colors.white.withOpacity(0)), //<-- SEE HERE
-                    ),
-                    border: UnderlineInputBorder(
-                      borderSide: BorderSide(
-                          width: 2,
-                          color: Colors.white.withOpacity(0)), //<-- SEE HERE
-                    ),
-                    hintText: "Select Transportation Type",
-                    hintStyle: TextStyle(
-                        color: kPrimaryDarkenedColor.withOpacity(0.5),
-                        fontWeight: FontWeight.bold)),
               ),
 
 
@@ -198,7 +169,11 @@ class _SuggestPlan extends State<SuggestPlan> {
                   Text('Travel Preference',style: TextStyle(
                     color: kSecondaryColor,
                     fontSize:18,
+<<<<<<< HEAD
                       fontWeight: FontWeight.bold,
+=======
+                    fontWeight: FontWeight.bold,
+>>>>>>> funravel_v0_gulin
 
                   ),),
 
@@ -208,6 +183,7 @@ class _SuggestPlan extends State<SuggestPlan> {
             SizedBox(
               height: 10,
             ),
+<<<<<<< HEAD
         Container(
           child: Row(
             children:[
@@ -221,10 +197,39 @@ class _SuggestPlan extends State<SuggestPlan> {
 
                       backgroundColor: MaterialStatePropertyAll<Color>(
                           kSecondaryColor),
+=======
+            Container(
+              child: Row(
+                children:[
+                  Container(
+                    width:160,
+                    height:40,
+                    child:
+                    ElevatedButton(
+
+                      style: ButtonStyle(
+
+                        backgroundColor: MaterialStatePropertyAll<Color>(
+                            kSecondaryColor),
+
+
+
+                      ),
+                      child: Text('Best Price',style: TextStyle(
+                        color: kPrimaryColor,
+
+                      ),),
+
+
+                      onPressed: () {},
+                    ),
+
+>>>>>>> funravel_v0_gulin
 
 
 
                   ),
+<<<<<<< HEAD
                   child: Text('Best Price',style: TextStyle(
                     color: kPrimaryColor,
 
@@ -251,10 +256,40 @@ class _SuggestPlan extends State<SuggestPlan> {
 
                       backgroundColor: MaterialStatePropertyAll<Color>(
                           kSecondaryColor),
+=======
+                  SizedBox(
+                    width: 40,
+                  ),
+                  Container(
+                    width:181,
+                    height:44,
+                    child:
+                    ElevatedButton(
+
+                      style: ButtonStyle(
+
+                        backgroundColor: MaterialStatePropertyAll<Color>(
+                            kSecondaryColor),
+
+
+
+                      ),
+                      child: Text('Shortest Travel Time',style: TextStyle(
+                        color: kPrimaryColor,
+
+
+                      ),),
+
+
+                      onPressed: () {},
+                    ),
+
+>>>>>>> funravel_v0_gulin
 
 
 
                   ),
+<<<<<<< HEAD
                   child: Text('Shortest Travel Time',style: TextStyle(
                     color: kPrimaryColor,
 
@@ -272,6 +307,11 @@ class _SuggestPlan extends State<SuggestPlan> {
             ],
           ),
         ),
+=======
+                ],
+              ),
+            ),
+>>>>>>> funravel_v0_gulin
             SizedBox(
               height: 10,
             ),
@@ -279,11 +319,19 @@ class _SuggestPlan extends State<SuggestPlan> {
 
               child: Row(
                 children:[
+<<<<<<< HEAD
             Container(
               height:40,
               width:160,
 
                 child:
+=======
+                  Container(
+                    height:40,
+                    width:160,
+
+                    child:
+>>>>>>> funravel_v0_gulin
                     ElevatedButton(
 
                       style: ButtonStyle(
@@ -306,10 +354,17 @@ class _SuggestPlan extends State<SuggestPlan> {
 
 
 
+<<<<<<< HEAD
             ),
               ],
               ),
       ),
+=======
+                  ),
+                ],
+              ),
+            ),
+>>>>>>> funravel_v0_gulin
             SizedBox(
               height: 10,
             ),
@@ -342,8 +397,13 @@ class _SuggestPlan extends State<SuggestPlan> {
 
                       style: ButtonStyle(
 
+<<<<<<< HEAD
                           backgroundColor: MaterialStatePropertyAll<Color>(
                               kSecondaryColor),
+=======
+                        backgroundColor: MaterialStatePropertyAll<Color>(
+                            kSecondaryColor),
+>>>>>>> funravel_v0_gulin
 
 
 
@@ -413,8 +473,12 @@ class _SuggestPlan extends State<SuggestPlan> {
                       side: BorderSide(width: 3, color: Colors.black),
                     ),
 
+                  ),
                 ),
+<<<<<<< HEAD
                 ),
+=======
+>>>>>>> funravel_v0_gulin
                 child: Text('Make Package',style: TextStyle(
                   color: kPrimaryColor,
 
@@ -427,6 +491,12 @@ class _SuggestPlan extends State<SuggestPlan> {
 
 
 
+<<<<<<< HEAD
+=======
+            ),
+            SizedBox(
+              height: 30,
+>>>>>>> funravel_v0_gulin
             ),
 
           ],
@@ -437,3 +507,23 @@ class _SuggestPlan extends State<SuggestPlan> {
     );
   }
 }
+
+buildCustomButton({ String? title, Function? function, Widget? sideWidget}){
+  return InkWell(
+    onTap: () => function != null ? function() : null,
+    child: Container(
+      padding: EdgeInsets.all(15),
+      margin: EdgeInsets.only(right: 35, left: 35),
+      color: kSecondaryColor,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(title ?? "", style: TextStyle(color: kPrimaryDarkenedColor, fontSize: 15, fontWeight: FontWeight.bold), ),
+          SizedBox(width: 4,),
+          sideWidget ?? Container()
+        ],
+      ),
+    ),
+  );
+}
+
